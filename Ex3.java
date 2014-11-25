5fba38048a04afde9a0ce286b418bdb062c3cf3b
n14001 <n14001@std.it-college.ac.jp>
2014-11-25 11:51:12 +0900

    finish

--- Ex3.java
+++ Ex3.java
@@ -11,74 +11,125 @@ class Ex3{
   }
 
   public int ex3_1(int n){
-    
-     if (n >= 0.0)
-       return n;
-     else
-       return -n;
+
+    if (n >= 0.0)
+      return n;
+    else
+      return -n;
   }
   public String ex3_2(int a, int b){
 
-    return "dummy";
+    if(a / b == a){
+        return b + "は" + a + "の約数です";
+    }else{
+        return b + "は" + a + "の約数ではありません";
+    }
   }
 
   public String ex3_3(int a, int b){
 
-    return "dummy";
+    if(b<a){
+        return a + "のほうが大きいです";
+    }else if(a<b){
+        return b + "のほうが大きいです";
+    }else{
+        return "同じ値です";
+    }
 
   }
 
   public String ex3_4(int a){
 
-    return "dummy";
+    if(a / 5 == 0){
+        return "その値は５で割り切れます";
+    }else if(a == -a){
+        return "正でない整数値です";
+    }else{
+        return "その値は５で割り切れません";
+    }
 
   }
 
   public String ex3_5(int a){
 
-    return "dummy";
+    if(a * 10 < a){
+        return "その値は10の倍数です";
+    }else if(a == -a){
+        return "その値は10の倍数ではありません";
+    }else{
+        return "正でない整数値です";
+    }
   }
 
   public String ex3_6(int a){
-    return "dummy";
+    if(a / 3 == 0){
+        return "その値は3で割り切れます";
+    }else if(a % 3 == 1){
+        return "その値を3で割った余りは1です";
+    }else if(a % 3 == 2){
+        return "その値を3で割った余りは2です";
+    }else if(a == -a){
+        return "正でない整数値です";
+    }else{
+        return "error";
+    }
   }
 
   public String ex3_7(int a){
-    return "dummy";
+    if(0 < 59){
+        return"不可";
+    }else if(60<69){
+        return"可";
+    }else if(70<79){
+        return"良";
+    }else if(80 < 100){
+        return"優";
+    }else{
+        return"範囲(0~100)外の値です";
+    }
+
   }
 
   public double ex3_8(double a, double b){
-    return -1.0;
+    
+    if(a < b){
+        return b;
+    }else{
+        return a;
+    }
   }
 
   public int ex3_9(int a, int b){
-    return 0;
+    int num = 0;
+    if(a < num || num < b){
+        return num;
+    }else{
+        return 1;
+    }
   }
 
-<<<<<<< HEAD
+
+
   public String ex3_10(int a, int b){
+    
+    if((a -b) < 10){
+        return"それらの差は10以下です";
+    }else{
+        return"それらの差は11位上です";
+    }
+  }
+
+  public int ex3_11(int a, int b, int c){
+    return 1000;
+  }
+
+  public int ex3_12(int a, int b, int c){
+    int[] intArray = {a, b, c};
+    return intArray[1];
+  }
+
+  public String ex3_13(int a){
     return "dummy";
   }
-=======
-	public int ex3_9(int a, int b){
-		return 0;
-	}
 
-	public String ex3_10(int a, int b){
-		return "dummy";
-	}
-
-	public int ex3_11(int a, int b, int c){
-		return 1000;
-	}
-
-	public int ex3_12(int a, int b, int c){
-		return 1000;
-	}
-
-	public String ex3_13(int a){
-		return "dummy";
-	}
->>>>>>> 6de455b2988fdd3a820e755d6dc3966f2ccb29de
 }
-
