e0458dfc5d5922caf947e651f9039c186a0582b2
n14001 <n14001@std.it-college.ac.jp>
2014-11-25 09:38:35 +0900

    1

--- Ex3.java
+++ Ex3.java
@@ -1,61 +1,62 @@
 class Ex3{
 
-	public String ex3_0(int n){
+  public String ex3_0(int n){
 
-          if(n>0){
-            return "その値は正です";
-          }else{
-            return "その値は０か負です";
-          }
+    if(n>0){
+      return "その値は正です";
+    }else{
+      return "その値は０か負です";
+    }
 
-	}
+  }
 
-	public int ex3_1(int n){
-		
-          if(n>0){}
+  public int ex3_1(int n){
+    
+     if (n >= 0.0)
+       return n;
+     else
+       return -n;
+  }
+  public String ex3_2(int a, int b){
 
-	}
+    return "dummy";
+  }
 
-	public String ex3_2(int a, int b){
+  public String ex3_3(int a, int b){
 
-		return "dummy";
-	}
+    return "dummy";
 
-	public String ex3_3(int a, int b){
+  }
 
-		return "dummy";
+  public String ex3_4(int a){
 
-	}
+    return "dummy";
 
-	public String ex3_4(int a){
+  }
 
-		return "dummy";
+  public String ex3_5(int a){
 
-	}
+    return "dummy";
+  }
 
-	public String ex3_5(int a){
+  public String ex3_6(int a){
+    return "dummy";
+  }
 
-		return "dummy";
-	}
-	
-	public String ex3_6(int a){
-		return "dummy";
-	}
+  public String ex3_7(int a){
+    return "dummy";
+  }
 
-	public String ex3_7(int a){
-		return "dummy";
-	}
+  public double ex3_8(double a, double b){
+    return -1.0;
+  }
 
-	public double ex3_8(double a, double b){
-		return -1.0;
-	}
+  public int ex3_9(int a, int b){
+    return 0;
+  }
 
-	public int ex3_9(int a, int b){
-		return 0;
-	}
-
-	public String ex3_10(int a, int b){
-		return "dummy";
-	}
+  public String ex3_10(int a, int b){
+    return "dummy";
+  }
 }
 
